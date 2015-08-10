0ce02ce2dffb670364024dbb8af379dc04050dcb
JustinTicktock <JustinTicktock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 21:39:18 +0000

    1.2 now marked stable
    git-svn-id: https://plugins.svn.wordpress.org/role-includer/trunk@1217629 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- role-includer.php
+++ role-includer.php
@@ -3,7 +3,7 @@
 Plugin Name: Role Includer
 Plugin URI: http://justinandco.com/plugins/role-includer
 Description: Allows users to be allocated multiple roles.
-Version: 1.1
+Version: 1.2
 Author: Justin Fletcher
 Author URI: http://justinandco.com
 Text Domain: role-includer
