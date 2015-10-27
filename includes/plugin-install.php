5c72cd2e0654383537e86ca5328fc6c5fee59c88
JustinTicktock <JustinTicktock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-27 17:04:54 +0000

    update text domain for language packs
    git-svn-id: https://plugins.svn.wordpress.org/role-includer/trunk@1274128 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/plugin-install.php
+++ includes/plugin-install.php
@@ -25,22 +25,22 @@ function role_includer_tgmpa_register( ) {
 					'menu_title'                      => __( 'Install Plugins', 'user-upgrade-capability' ),
 					'installing'                      => __( 'Installing Plugin: %s', 'user-upgrade-capability' ), // %s = plugin name.
 					'oops'                            => __( 'Something went wrong with the plugin API.', 'user-upgrade-capability' ),
-					'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'user-upgrade-capability' ), // %1$s = plugin name(s).
-					'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'user-upgrade-capability' ),
-					'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'user-upgrade-capability' ),
-					'return'                          => __( 'Return to Required Plugins Installer', 'user-upgrade-capability' ),
-					'dashboard'                      => __( 'Return to the dashboard', 'user-upgrade-capability' ),
-					'plugin_activated'                => __( 'Plugin activated successfully.', 'user-upgrade-capability' ),
-					'activated_successfully'         => __( 'The following plugin was activated successfully:', 'user-upgrade-capability' ),
-					'complete'                        => __( 'All plugins installed and activated successfully. %s', 'user-upgrade-capability' ), // %s = dashboard link.
-					'dismiss'                        => __( 'Dismiss this notice', 'user-upgrade-capability' ),
+					'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'role-includer' ), // %1$s = plugin name(s).
+					'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'role-includer' ), // %1$s = plugin name(s).
+					'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'role-includer' ), // %1$s = plugin name(s).
+					'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'role-includer' ), // %1$s = plugin name(s).
+					'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'role-includer' ), // %1$s = plugin name(s).
+					'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'role-includer' ), // %1$s = plugin name(s).
+					'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'role-includer' ), // %1$s = plugin name(s).
+					'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'role-includer' ), // %1$s = plugin name(s).
+					'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'role-includer' ),
+					'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'role-includer' ),
+					'return'                          => __( 'Return to Required Plugins Installer', 'role-includer' ),
+					'dashboard'                      => __( 'Return to the dashboard', 'role-includer' ),
+					'plugin_activated'                => __( 'Plugin activated successfully.', 'role-includer' ),
+					'activated_successfully'         => __( 'The following plugin was activated successfully:', 'role-includer' ),
+					'complete'                        => __( 'All plugins installed and activated successfully. %s', 'role-includer' ), // %s = dashboard link.
+					'dismiss'                        => __( 'Dismiss this notice', 'role-includer' ),
 					'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
 			)
 	);
